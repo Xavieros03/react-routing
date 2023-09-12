99484bccb57dfe83752633f67eb76ccf826efed9
Xavier Ortega <xaviersendra@hotmail.com>
2023-09-12 19:51:45 +0200

    exercise

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css'
 import Home from './components/Home.jsx'
 import Navbar from './components/Navbar.jsx'
+import MoviesList from "./components/MoviesList"
+import About from "./components/About"
+import { Route, Routes } from "react-router-dom"
 
 function App() {
 	return (
 		<div className='App'>
 			<Navbar />
 			<Home />
+			<Routes>
+				<Route path='/movies' element={<MoviesList />} />
+				<Route path='/about' element={<About />} />
+			</Routes>
 		</div>
 	)
 }
